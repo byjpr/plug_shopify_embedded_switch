13972bd967d5ad9bda9b9e27c0113b5e6f202576
Jordan Parker <jgit@fmail.co.uk>
2021-07-17 00:44:39 +0100

    fork from Enron

--- lib/plug_shopify_embedded_switch.ex
+++ lib/plug_shopify_embedded_switch.ex
@@ -1,18 +1,27 @@
 defmodule PlugShopifyEmbeddedSwitch do
   @moduledoc """
-  Documentation for `PlugShopifyEmbeddedSwitch`.
+  Switches auth between URL Params and JWT, designed for Shopify App Bridge
   """
+  import Plug.Conn
 
-  @doc """
-  Hello world.
+  @spec init(any) :: any
+  def init(options), do: options
 
-  ## Examples
+  @spec call(Plug.Conn.t(), any) :: Plug.Conn.t()
+  def call(conn, _) do
+    origin_url_params = PlugShopifyEmbeddedSwitch.Helper.get_param_from_url(conn, :shop)
 
-      iex> PlugShopifyEmbeddedSwitch.hello()
-      :world
+    if origin_url_params do
+      conn
+      |> put_private(:shopify_url_config, origin_url_params)
+      |> put_private(:shop_origin_type, :url)
+    else
+      origin_header_params =
+        PlugShopifyEmbeddedSwitch.Helper.get_param_from_header(conn, "authorization")
 
-  """
-  def hello do
-    :world
+      conn
+      |> put_private(:shopify_jwt_config, origin_header_params)
+      |> put_private(:shop_origin_type, :jwt)
+    end
   end
 end
