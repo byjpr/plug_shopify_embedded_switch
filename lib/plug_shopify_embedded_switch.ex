e39d248343e9930caa8704bdbcb8d824563a2dc6
Jordan Parker <jgit@fmail.co.uk>
2021-07-17 00:38:20 +0100

    mix new

---
+++ lib/plug_shopify_embedded_switch.ex
@@ -0,0 +1,18 @@
+defmodule PlugShopifyEmbeddedSwitch do
+  @moduledoc """
+  Documentation for `PlugShopifyEmbeddedSwitch`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlugShopifyEmbeddedSwitch.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plug_shopify_embedded_switch_test.exs
@@ -0,0 +1,8 @@
+defmodule PlugShopifyEmbeddedSwitchTest do
+  use ExUnit.Case
+  doctest PlugShopifyEmbeddedSwitch
+
+  test "greets the world" do
+    assert PlugShopifyEmbeddedSwitch.hello() == :world
+  end
+end
