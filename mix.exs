c48e619c244eb2f102d90dac8ba3aa6516290c44
Jordan Parker <jgit@fmail.co.uk>
2021-07-18 09:34:31 +0100

    add ex_doc

--- mix.exs
+++ mix.exs
@@ -39,7 +39,8 @@ defmodule PlugShopifyEmbeddedSwitch.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.5.4"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
